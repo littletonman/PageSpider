a9baf7848741c618230809be337b0d592cef310c
tianyu.x.zhu <tianyu.x.zhu@oracle.com>
2017-09-19 17:59:18 -0700

    change format

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,12 +1,13 @@
-def load_urls_from_file(file_path:str):
+def load_urls_from_file(file_path: str):
     # TODO: add the code that reads the text file with the urls in it
     pass
 
+
 def load_page(url: str):
-    #TODO: add the code that reads the url
+    # TODO: add the code that reads the url
     pass
+
 
 def scrape_page(page_contents: str):
     # TODO: add the code that reads the url
     pass
-
